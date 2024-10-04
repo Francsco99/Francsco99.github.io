e3ef12384acf9ba8971a68c3d2992c82bf180f7d
fspezzano <fra.spezzano@stud.uniroma3.it>
2024-10-04 12:20:10 +0200

    nuova pagina portfolio e messi script in una cartella

---
+++ script/diy_images.js
@@ -0,0 +1,8 @@
+function changePhoto(photoSrc) {
+    const largePhoto = document.getElementById("largePhoto");
+    largePhoto.style.opacity = 0; // Inizia la dissolvenza
+    setTimeout(() => {
+      largePhoto.src = photoSrc;
+      largePhoto.style.opacity = 1; // Ripristina l'opacità
+    }, 500); // Durata della dissolvenza deve coincidere con la durata della transizione
+  }
