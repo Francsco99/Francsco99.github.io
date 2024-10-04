7c84b963235343fa10138350057a48a4562dea5a
fspezzano <fra.spezzano@stud.uniroma3.it>
2024-10-04 14:29:46 +0200

    nuova pagina per elementi diy

--- script/diy_images.js
+++ script/diy_images.js
@@ -1,8 +1,9 @@
+// Funzione per cambiare l'immagine grande
 function changePhoto(photoSrc) {
-    const largePhoto = document.getElementById("largePhoto");
-    largePhoto.style.opacity = 0; // Inizia la dissolvenza
-    setTimeout(() => {
+  const largePhoto = document.getElementById("largePhoto");
+  largePhoto.style.opacity = 0; // Inizia la dissolvenza
+  setTimeout(() => {
       largePhoto.src = photoSrc;
       largePhoto.style.opacity = 1; // Ripristina l'opacità
-    }, 500); // Durata della dissolvenza deve coincidere con la durata della transizione
-  }
+  }, 300); // Durata della dissolvenza deve coincidere con la durata della transizione
+}
