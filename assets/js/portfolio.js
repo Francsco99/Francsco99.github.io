1fa6fb2cd30b5bf4ebf458d3f9609b0957725f3c
fspezzano <fra.spezzano@stud.uniroma3.it>
2024-11-13 17:00:59 +0100

    file nuovi

---
+++ assets/js/portfolio.js
@@ -0,0 +1,40 @@
+var $grid = $('.grid').imagesLoaded( function() {
+    $grid.isotope({
+      itemSelector: '.grid-item',
+      masonry: {
+        columnWidth: 10,
+        gutter: 5
+      }
+    });
+  });
+  
+// Filtraggio
+$('.button').on('click', function () {
+  var filterValue = $(this).attr('data-filter');
+  $grid.isotope({ filter: filterValue });
+  $('.button').removeClass('active');
+  $(this).addClass('active');
+  
+  // Cambia il lightbox group in base al filtro
+  if (filterValue === '*') {
+      // Seleziona tutte le immagini e imposta 'all' come gruppo Lightbox
+      $('a[data-lightbox]').attr('data-lightbox', 'all');
+  } else {
+      // Filtra e imposta il gruppo Lightbox basato sulla classe
+      $('a[data-lightbox]').each(function () {
+      var itemClasses = $(this).parent().attr('class');
+      if (itemClasses.includes(filterValue.slice(1))) {
+          $(this).attr('data-lightbox', filterValue.slice(1)); // Assegna il gruppo corretto
+      } else {
+          $(this).attr('data-lightbox', ''); // Rimuovi il gruppo dalle altre immagini
+      }
+      });
+  }
+  });
+  
+  // Inizializza Lightbox
+  lightbox.option({
+  'resizeDuration': 200,
+  'wrapAround': true,
+  'alwaysShowNavOnTouchDevices': true
+  });
