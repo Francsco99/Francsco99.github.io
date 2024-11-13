3a5e1e101bedbf0aefda1ebb56db9e58dc053280
fspezzano <fra.spezzano@stud.uniroma3.it>
2024-11-13 19:09:36 +0100

    fix danni di prima

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-menu");
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const closeIcon = document.querySelector(".close-icon");
+
+  // Aggiungi o rimuovi la classe 'open' per aprire o chiudere il menu
   menu.classList.toggle("open");
-  icon.classList.toggle("open");
- 
+  hamburgerIcon.classList.toggle("open");
+  closeIcon.classList.toggle("open");
 }
