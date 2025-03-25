4de528d3577af5978643fcaa609dc3bb3c824eb0
fspezzano <fra.spezzano@stud.uniroma3.it>
2025-03-25 18:59:06 +0100

    removed initial screen

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -28,27 +28,3 @@ video.addEventListener('loadedmetadata', () => {
     });
   }
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-  const overlay = document.getElementById("overlay");
-
-  // Aggiungi animazione bounce in loop per il testo
-  gsap.to("#overlay p", {
-    opacity: 0,
-    duration: 0.8,
-    ease: "power1.inOut",
-    repeat: -1,   // Loop infinito
-    yoyo: true    // Effetto fade in e fade out
-  });
-
-  overlay.addEventListener("click", function() {
-    gsap.to(overlay, {
-      duration: 2,
-      opacity: 0,
-      ease: "power2.out",
-      onComplete: function() {
-        overlay.style.display = "none";
-      }
-    });
-  });
-});
